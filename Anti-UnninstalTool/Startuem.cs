7c2f5249d67aaf5e7ecc7f37d69aad56cc1a9a73
NotAFrizik <frizikcreate@gmail.com>
2024-06-05 23:03:53 +0700

    Update Startuem.cs

--- Anti-UnninstalTool/Startuem.cs
+++ Anti-UnninstalTool/Startuem.cs
@@ -1,57 +1,28 @@
-﻿using System;
+ using System;
+using System.Diagnostics;
 using System.IO;
-using Microsoft.Win32;
-using System.Windows.Forms;
 
-namespace Anti_UnninstalTool
+class Program
 {
-   /******************************************
-   *                                        *
-   *  Created by NotAFrizik                 *
-   *  Visit: https://github.com/NotAFrizik  *
-   *                                        *
-   ******************************************/
-
-    internal class Startuem
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            string driverPath = @"C:\Windows\System32\drivers\CisUtMonitor.sys";
-            string serviceName = "CisUtMonitor";
+        string driverPath = @"C:\Windows\System32\drivers\CisUtMonitor.sys";
+        string serviceName = "CisUtMonitor";
 
-            if (File.Exists(driverPath))
+        try
+        {
+           string command = $"sc delete {serviceName}";
+           ProcessStartInfo startInfo = new ProcessStartInfo
             {
-                try
-                {
-                    using (RegistryKey key = Registry.LocalMachine.OpenSubKey($@"SYSTEM\CurrentControlSet\Services\{serviceName}"))
-                    {
-                        string command = $"sc delete {serviceName}";
-                        System.Diagnostics.Process process = new System.Diagnostics.Process();
-                        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
-                        {
-                            WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
-                            FileName = "cmd.exe",
-                            Arguments = $"/c {command}",
-                            RedirectStandardOutput = true,
-                            UseShellExecute = false,
-                            CreateNoWindow = true
-                        };
-                        process.StartInfo = startInfo;
-                        process.Start();
-                        process.WaitForExit();
-                    }
- 
-                    File.Delete(driverPath);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"{ex.Message}");
-                }
-            }
-            else
-            {
-                MessageBox.Show($"Драйвер {driverPath} не найден.");
-            }
-        }
-    }
-}
+                FileName = "cmd.exe",
+                Arguments = $"/c {command}",
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+             };
+             Process process = Process.Start(startInfo);
+             process.WaitForExit();
+           
+            File.Delete(driverPath);
+         }
+      }
+      catch {}
